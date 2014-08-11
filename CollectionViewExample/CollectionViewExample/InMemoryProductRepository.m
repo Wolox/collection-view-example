eae6fb78ef708865a74dba5f17a008c0fd09e03d
Guido Marucci Blas <guidomb@gmail.com>
2014-08-10 21:00:13 -0300

    Simulates async latency in InMemoryProductRepository
    Delays the execution of the code by 200ms to simulate
    API latency.

--- CollectionViewExample/CollectionViewExample/InMemoryProductRepository.m
+++ CollectionViewExample/CollectionViewExample/InMemoryProductRepository.m
@@ -9,9 +9,16 @@
 #import "InMemoryProductRepository.h"
 #import "ProductFactory.h"
 
+#define REPOSITORY_LATENCY 200 //milleseconds
+
 NSError * productNotFoundError(NSString * productId) {
     NSDictionary * info = @{@"productId" : productId};
     return [NSError errorWithDomain:PRODUCT_REPOSITORY_ERROR_DOMAIN code:PRODUCT_NOT_FOUND userInfo:info];
+}
+
+void simulateAsyncRequest(void(^block)()) {
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, REPOSITORY_LATENCY * NSEC_PER_MSEC), queue, block);
 }
 
 @interface InMemoryProductRepository ()
@@ -40,14 +47,14 @@ NSError * productNotFoundError(NSString * productId) {
 
 - (void)fetchProductsWithHandler:(void(^)(NSError *, NSArray *))handler {
     __block typeof(self) this = self;
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    simulateAsyncRequest(^{
         handler(nil, [this.data allValues]);
     });
 }
 
 - (void)fetchById:(NSString *)productId withHandler:(void(^)(NSError *, Product *))handler {
     __block typeof(self) this = self;
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    simulateAsyncRequest(^{
         Product * product = this.data[productId];
         if (product) {
             handler(nil, product);
@@ -58,7 +65,7 @@ NSError * productNotFoundError(NSString * productId) {
 }
 
 - (void)favoriteProductWithId:(NSString *)productId withHandler:(void(^)(NSError *))handler {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    simulateAsyncRequest(^{
         handler(nil);
     });
 }
