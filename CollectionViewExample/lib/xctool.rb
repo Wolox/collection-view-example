0b3069bf027ee923ec551254e3159c3707b8e2e1
Guido Marucci Blas <guidomb@gmail.com>
2014-08-20 15:00:27 -0300

    Refactor XCTool Ruby class #2.
    Removes method missing usage.

--- CollectionViewExample/lib/xctool.rb
+++ CollectionViewExample/lib/xctool.rb
@@ -1,43 +1,91 @@
 class XCTool
 
+  ACTIONS = %w(
+    clean
+    build
+    build-tests
+    archive
+    analyze
+    install
+  )
+
+  XCTOOL_CMD = "xctool"
+
+  class << self
+
+    def to_action_method_name(action)
+      if ACTIONS.include?(action)
+        action.downcase.gsub("-", "_")
+      end
+    end
+
+  end
+
   attr_reader :workspace, :scheme, :xctool_path
 
-  def initialize(workspace, scheme, xctool_path = nil)
+  def initialize(workspace, scheme, opts = {})
     @workspace = workspace
     @scheme = scheme
-    @xctool_path = xctool_path
+    @xctool_path = Pathname.new(opts[:xctool_path] || "")
+    opts = {verbose: false, dry_run: false}.merge(opts)
+    @verbose = opts[:verbose]
+    @dry_run = opts[:dry_run]
   end
 
   def run(action, arguments = {}, options = {})
-    opts = default_base_options.merge(options)
-    cmd = "#{xctool} #{build_string(opts)} #{action} #{build_string(arguments)}".strip
-    log("Running command '#{cmd}'")
-    system(cmd) unless ENV['DRY']
+    if valid_action?(action)
+      opts = default_base_options.merge(options)
+      command = build_command(action, arguments, opts)
+      execute(command)
+    else
+      raise "Invalid action #{action}"
+    end
   end
 
-  def method_missing(method, *args, &block)
-    send(:run, method, *args)
+  def dry_run?
+    @dry_run
+  end
+
+  def verbose?
+    @verbose
+  end
+
+  ACTIONS.each do |action|
+    define_method(to_action_method_name(action)) do |*args|
+      run(action, *args)
+    end
   end
 
   private
 
+    def build_command(action, arguments, options)
+      options = to_options_string(options)
+      arguments = to_options_string(arguments)
+      "#{xctool} #{options} #{action} #{arguments}".strip
+    end
+
+    def execute(command)
+      log("Executing command '#{command}'")
+      system(command) unless dry_run?
+    end
+
+    def valid_action?(action)
+      ACTIONS.include?(action)
+    end
+
     def log(message)
-      puts message if ENV['VERBOSE']
+      puts "#{self.class}: #{message}" if verbose?
     end
 
     def xctool
-      if xctool_path
-        "#{xctool_path}/xctool"
-      else
-        "xctool"
-      end
+      xctool_path + XCTOOL_CMD
     end
 
     def default_base_options
       @default_base_options ||= { workspace: workspace, scheme: scheme }
     end
 
-    def build_string(options = {})
+    def to_options_string(options = {})
       options.reduce("") do |options_string, (key, value)|
         if value
           options_string + " -#{key} #{value}"
